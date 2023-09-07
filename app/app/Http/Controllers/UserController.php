6ba413c71932c7a8400cdd7b81c6026212cbf82b
IvnK <karshev.ivan09.07.38@gmail.com>
2023-09-07 20:25:39 +0700

    add join in GetUser

--- app/app/Http/Controllers/UserController.php
+++ app/app/Http/Controllers/UserController.php
@@ -56,6 +56,12 @@ class UserController extends Controller
 
     public function GetUser(){
         $response = User::all();
+
+        $response = $users = DB::table('users')
+            ->join('organizations', 'users.organization_id', '=', 'organizations.id')
+            ->select('users.*','organizations.name as organization_name')
+            ->get();
+
         print_r( json_encode($response) );
     }
 }
